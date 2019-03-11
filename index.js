5ac6cfb07c1601239095c04e4993fa8d13772e0a
sam.jheng <m3gl4a@gmail.com>
2019-03-11 15:23:25 +0800

    [測試機器人回話]

--- index.js
+++ index.js
@@ -5,16 +5,25 @@ const lineSetting = {
     channelSecret: '8ce86563f45b99dedc137f7a984ca9ea',
     channelAccessToken: '85Bj12LVNLWtyDXRenIqMoR7ue6dOLJdOJ9UZDTZI7oWMDdwMKEG/Q7+jr10YdqVrr9EehJemL3mZ+Mq8hFZwMQutm4EgK4L1Iv6XCg82kGddIwOrInwH2czLiPEwO/VmnihJ03sXW+mDTUW9YKeXAdB04t89/1O/w1cDnyilFU='
 }
-var bot = linebot(lineSetting);
-bot.on('message', function (event) {
-    console.log(event); //把收到訊息的 event 印出來看看
-});
+
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
-
+var bot = linebot(lineSetting);
+bot.on('message', function (event) {
+    // event.message.text是使用者傳給bot的訊息
+    // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
+    var replyMsg = `Hello你剛才說的是:${event.message.text}`;
+    event.reply(replyMsg).then(function (data) {
+    // 當訊息成功回傳後的處理
+    }).catch(function (error) {
+        // 當訊息回傳失敗後的處理
+        console.log(error);
+    });
+});
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
+    console.log('[BOT已準備就緒]');
     console.log("App now running on port", port);
 });
